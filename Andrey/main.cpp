823e112e6d389262c2e2431705ea3013e24d9577
Darking_Andrey <86359412+Santas7@users.noreply.github.com>
2022-04-21 23:14:56 +0400

    Update main.cpp

--- Andrey/main.cpp
+++ Andrey/main.cpp
@@ -86,22 +86,33 @@ private:
     int N;
     Figure** _A;
 public:
-    void ADD(Figure* obj) {
+    void Add(Figure* obj) {
         ++N;
         _A = (Figure**)realloc(_A, N * sizeof(Figure*));
         _A[N - 1] = obj;
     }
-};
-void Oper(Figure** _A, int N) {
-    for (int i = 0; i <= N; ++i) {
-        _A[i]->print();
-        cout << "Surface area: " << _A[i]->area() << endl << "Volume: " << _A[i]->volume() << endl << endl;
+    void Print() const
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            _A[i]->print();
+            cout << "Surface area: " << _A[i]->area() << endl << "Volume: " << _A[i]->volume() << endl << endl;
+        }
+        cout << "[0] Back-->";
     }
-    cout << "Back-->";
-}
+    void del(int index) {
+        delete _A[index];
+    }
+    void delAll() {
+        for (int i = 9; i < N; ++i) {
+            delete _A[i];
+        }
+    }
+};
+
 int Menu() {
     int key = _getch();
-    if (key == 49 || key == 50 || key == 51 || key == 52)
+    if (key == 49 || key == 50 || key == 51 || key == 52 || key == 53 || key == 54 || key == 55)
         return key;
     return 0;
 }
@@ -113,18 +124,18 @@ int main()
     while (true) {
         int commandKey = Menu();
         switch (commandKey) {
-        case 49:
+        case 49: 
             system("cls");
             cout << "Input side--> "; cin >> a; 
-            coll->ADD(new Cube(a));
+            coll->Add(new Cube(a));
             system("cls");
             cout << "Added! [0] Back -->";
             break;
-        case 50:
+        case 50: 
             system("cls");
             Figure* _A[2];
             cout << "Input radius--> "; cin >> a; 
-            coll->ADD(new Sphere(a));
+            coll->Add(new Sphere(a));
             system("cls");
             cout << "Added! [0] Back -->";
             break;
@@ -133,17 +144,33 @@ int main()
             cout << "Input radius--> "; cin >> a; 
             cout << "Input length--> "; cin >> b; 
             cout << "Input height--> "; cin >> c; 
-            coll->ADD(new Cone(a, b, c));
+            coll->Add(new Cone(a, b, c));
             system("cls");
             cout << "Added! [0] Back -->";
             break;
         case 52:
             system("cls");
-            //Oper(_A, N);
+            coll->Print();
+            break;
+        case 53:
+            system("cls");
+            cout << "Random";
+            break;
+        case 54:
+            system("cls");
+            cout << "Input index value--> "; cin >> a;
+            coll->del(a);
+            system("cls");
+            cout << "Object is Deleted! [0] Back-->";
+            break;
+        case 55:
+            system("cls");
+            coll->delAll();
+            cout << "All Objects is Deleted! [0] Back-->";
             break;
         case 0:
             system("cls");
-            cout << "[1] Add Cube" << endl << "[2] Add Sphere" << endl << "[3] Add Cone" << endl << "[4] Output all list figures" << endl << "-->";
+            cout << "[1] Add Cube" << endl << "[2] Add Sphere" << endl << "[3] Add Cone" << endl << "[4] Output all list figures" << endl << "[5] Inserting a randomly generated object at the end of a collection" << endl << "[6] Deleting one object by index" << endl << "[7] Deleting all objects" << endl << "-->";
             break;
         default:
             system("cls");
